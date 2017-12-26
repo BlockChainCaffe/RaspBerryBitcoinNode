990d800387aa40c2a92a5de4363f5b2fc5f19eda
BlockChain Caffè <32158747+BlockChainCaffe@users.noreply.github.com>
2017-12-27 00:18:33 +0100

    Update Comandi.sh

--- Comandi.sh
+++ Comandi.sh
@@ -3,10 +3,16 @@
 
 ### Avvio Manuale
 	
-	bitcoind -datadir=/opt/bitcoinData -daemon
+	bitcoind -datadir=/opt/bitcoinData -conf=/root/.bitcoin/bitcoin.conf -daemon
+	
+### Comando base
+
+	bitcoin-cli -rpcuser=berrynode -rpcpassword=berrynode -rpcconnect=127.0.0.1 -rpcport=8332 ...
 
 ### Comandi da CLI
 
-	bitcoin-cli getinfo
+	bitcoin-cli help
 	bitcoin-cli stop
 	bitcoin-cli getblockcount
+	bitcoin-cli getblockchaininfo
+	
